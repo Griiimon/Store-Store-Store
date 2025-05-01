af5309b32dfcebb9f3ae7ac8b1a3ccbd601e6ab5
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-05-01 10:07:55 +0200

    Update carrying_player_state.gd

--- player/state machine/carrying_player_state.gd
+++ player/state machine/carrying_player_state.gd
@@ -9,13 +9,15 @@ func on_physics_process(delta: float):
 	if Input.is_action_just_pressed("drop"):
 		drop.emit()
 		return
+		
 	default_movement(delta)
+	default_input()
 
 
 func restore_model_transform()-> bool:
 	var query:= PhysicsShapeQueryParameters3D.new()
 	query.collision_mask= CollisionLayers.CRATES
-	query.transform= player.carry_large_offset.global_transform
+	query.transform= player.carried_crate.global_transform
 	query.shape= player.carried_crate.collision_shape.shape
 
 	var result= player.get_world_3d().direct_space_state.intersect_shape(query)
